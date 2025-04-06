ce5d1af38940018880f4370ddcaf97075032f088
tritotsripol <tritotsripol@gmail.com>
2025-04-06 18:45:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+function changeLanguage(language) {
+  document.querySelectorAll('[data-' + language + ']').forEach(function(element) {
+    element.textContent = element.getAttribute('data-' + language);
+  });
+}
+
+changeLanguage('en');
+
+// theme
+const themeToggleButton = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+const prismTheme = document.getElementById('prism-theme');
+
+// ตรวจสอบธีมที่บันทึกไว้
+let savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'light') {
+  document.body.classList.add('light');
+  document.body.classList.remove('dark');
+  themeIcon.src = 'sun-icon.png'; // ไอคอนพระอาทิตย์ (สว่าง)
+} else {
+  document.body.classList.add('dark');
+  document.body.classList.remove('light');
+  themeIcon.src = 'moon-icon.png'; // ไอคอนพระจันทร์ (มืด)
+}
+
+// ฟังก์ชันเปลี่ยนธีม
+themeToggleButton.addEventListener('click', () => {
+  const isLight = document.body.classList.toggle('light');
+  document.body.classList.toggle('dark', !isLight);
+
+  if (isLight) {
+    localStorage.setItem('theme', 'light');
+    themeIcon.src = 'sun-icon.png'; 
+  } else {
+    localStorage.setItem('theme', 'dark');
+    themeIcon.src = 'moon-icon.png'; 
+  }
+});
+
+//download
+function downloadFiles() {
+  const files = [
+    'page1.png',
+    'page2.png',
+    'page3.png'
+  ];
+
+  files.forEach(file => {
+    const a = document.createElement('a');
+    a.href = file;
+    a.download = file.split('/').pop(); // ใช้ชื่อไฟล์จาก URL
+    a.click(); // คลิกเพื่อดาวน์โหลด
+  });
+}
+
+//gotop
+const gotop = document.querySelector('.gotop');
+gotop.addEventListener('click',function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
